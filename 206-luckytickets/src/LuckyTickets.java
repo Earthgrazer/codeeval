5aaf4fc1fb138b886072676b9ebcacd9c336b572
Eric Chen <echen@inetco.com>
2015-11-26 17:32:28 -0800

    Mostly working, but still conversion to use BigInteger.

--- 206-luckytickets/src/LuckyTickets.java
+++ 206-luckytickets/src/LuckyTickets.java
@@ -1,6 +1,5 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
+import java.math.BigInteger;
 
 public class LuckyTickets {
     public static void main(String[] args) throws java.lang.Exception {
@@ -18,14 +17,47 @@ public class LuckyTickets {
         System.exit(0);
     }
 
-    private static int getLuckyCount(int numDigits)
+    private static BigInteger getLuckyCount(int numDigits)
     {
-        int[][] permutations = new int[9 * numDigits][numDigits];
+        BigInteger[][] permutations = new BigInteger[9 * numDigits][numDigits];
 
-        // initialize all permutations to 0
-        for (int numIndex = 0; numIndex < permutations.length; numIndex++) {
-            for (int digitIndex = 0; digitIndex < permutations[0].length; digitIndex++) {
-                permutations[numIndex][digitIndex] = 0;
+        for (int digits = 0; digits < numDigits; digits++) {
+            for (int num = 0; num < numDigits * 9; num++) {
+                permutations[num][digits] = BigInteger.ZERO;
+            }
+        }
+
+        // brute force method, disabled
+        for (int digits = 0; false && digits < numDigits; digits++) {
+            for (int num = 0; num < numDigits * 9; num++) {
+                int[] combos = new int[digits + 1];
+                int[] dupes = new int[(int) Math.pow(10, digits + 1)];
+                BigInteger ways = BigInteger.ZERO;
+                for (int iter = 0; iter < Math.pow(10, digits + 1); iter++) {
+                    int sum = 0;
+                    int seq = 0;
+
+                    for (int digit = 0; digit <= digits; digit++) {
+                        sum += combos[digit];
+                        seq += Math.pow(10, digit) * combos[digit];
+                    }
+
+                    if (dupes[seq] == 0 && sum == num + 1) {
+                        ways.add(BigInteger.ONE);
+                        dupes[seq] = 1;
+                    }
+
+                    for (int digit = 0; digit <= digits; digit++) {
+                        if (iter % Math.pow(10, digit) == 0) {
+                            if (combos[digit] == 9)
+                                combos[digit] = 0;
+                            else
+                                combos[digit]++;
+                        }
+                    }
+                }
+
+                permutations[num][digits] = ways;
             }
         }
 
@@ -33,17 +65,17 @@ public class LuckyTickets {
             for (int num = 0; num < 9; num++) {
                 // 1 digit
                 if (digits == 0) {
-                    permutations[num][digits] = 1;
+                    permutations[num][digits] = BigInteger.ONE;
                     continue;
                 }
 
-                int perms = 0;
+                BigInteger perms = BigInteger.ZERO;
 
                 for (int subNum = 0; subNum <= num; subNum++) {
-                    perms += permutations[subNum][digits - 1];
+                    perms.add(permutations[subNum][digits - 1]);
                 }
 
-                perms += 1;
+                perms.add(BigInteger.ONE);
 
                 permutations[num][digits] = perms;
             }
@@ -54,21 +86,62 @@ public class LuckyTickets {
             int maxNum = (digits + 1) * 9;
 
             for (int num = minNum; num < maxNum; num++) {
-                int perms = 0;
+                BigInteger perms = BigInteger.ZERO;
                 for (int num2 = minNum - maxNum + num; num2 < minNum; num2++) {
-                    perms += permutations[num2][digits - 1];
+                    perms.add(permutations[num2][digits - 1]);
                 }
 
                 permutations[num][digits] = perms;
             }
+
+            for (int nextDigits = digits + 1; nextDigits < numDigits; nextDigits++) {
+                for (int nextNum = minNum; nextNum < maxNum; nextNum++) {
+                    BigInteger perms = BigInteger.ZERO;
+                    for (int num2 = 0; num2 < 10; num2++) {
+                        perms.add(permutations[nextNum - num2][nextDigits - 1]);
+                    }
+                    permutations[nextNum][nextDigits] = perms;
+                }
+            }
         }
 
-        int totalPermCount = 0;
+        BigInteger maxPerm = BigInteger.ZERO;
 
-        for (int numIndex = (numDigits - 1) * 9; numIndex < permutations.length; numIndex++) {
-            totalPermCount += permutations[numIndex][numDigits - 1];
+        for (int numIndex = 0; numIndex < permutations.length; numIndex++) {
+            if (permutations[numIndex][numDigits - 1].compareTo(maxPerm) > 0)
+                maxPerm = permutations[numIndex][numDigits - 1];
         }
 
-        return totalPermCount;
+        //writePermutationsToFile(permutations);
+
+        return maxPerm;
+    }
+
+    private static void writePermutationsToFile(int[][] permutations)
+    {
+        try {
+            PrintWriter writer = new PrintWriter("output.csv");
+
+            StringBuffer header = new StringBuffer("num,");
+
+            for (int digit = 1; digit <= permutations[0].length; digit++) {
+                header.append(digit + ",");
+            }
+
+            writer.println(header);
+
+            for (int num = 0; num < permutations.length; num++) {
+                writer.print(num + ",");
+                for (int digit = 0; digit < permutations[num].length; digit++) {
+                    writer.print(permutations[num][digit] + ",");
+                }
+
+                writer.println();
+            }
+
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
